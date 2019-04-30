2c335e679f11864c7da3e1f02be3b6028787f2ac
oszkarpap <scaevola.c@gmail.com>
2019-04-30 19:20:46 +0200

    4th add Fish class and Cars classes

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 package com.timbuchalka;
 
-public class Main {
+public class Main extends Object{
 
     public static void main(String[] args) {
-	    Animal animal = new Animal("Animal", 1, 1, 5, 5);
 
-        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
-//        dog.eat();
-        dog.walk();
-//        dog.run();
+	    // Challenge.
+        // Start with a base class of a Vehicle, then create a Car class that inherits from this base class.
+        // Finally, create another class, a specific type of Car that inherits from the Car class.
+        // You should be able to hand steering, changing gears, and moving (speed in other words).
+        // You will want to decide where to put the appropriate state and behaviours (fields and methods).
+        // As mentioned above, changing gears, increasing/decreasing speed should be included.
+        // For you specific type of vehicle you will want to add something specific for that type of car.
 
     }
 }
