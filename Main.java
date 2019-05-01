93b101a6523788f66b1598df9b56934f92e4bdf5
oszkarpap <scaevola.c@gmail.com>
2019-05-01 09:40:34 +0200

    5th change Main to

--- Main.java
+++ Main.java
@@ -3,20 +3,16 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
+	    Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("2208", "Dell", "240", dimensions);
 
-        // Challenge.
-        // Start with a base class of a Vehicle, then create a Car class that inherits from this base class.
-        // Finally, create another class, a specific type of Car that inherits from the Car class.
-        // You should be able to hand steering, changing gears, and moving (speed in other words).
-        // You will want to decide where to put the appropriate state and behaviours (fields and methods).
-        // As mentioned above, changing gears, increasing/decreasing speed should be included.
-        // For you specific type of vehicle you will want to add something specific for that type of car.
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
 
-        Outlander outlander = new Outlander(36);
-        outlander.steer(45);
-        outlander.accelerate(30);
-        outlander.accelerate(20);
-        outlander.accelerate(-42);
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
 
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+        thePC.getMonitor().drawPixelAt(1500, 1200, "red" );
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
     }
 }
