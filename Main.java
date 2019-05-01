8e515f0dcd52fd3a509d089aa7b14916d6e9faaf
oszkarpap <scaevola.c@gmail.com>
2019-05-01 09:43:34 +0200

    7. change

--- Main.java
+++ Main.java
@@ -3,43 +3,21 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-        Dimensions dimensions = new Dimensions(20, 20, 5);
-	    Case theCase = new Case("220B", "Dell", "240", dimensions);
+//	    Player player = new Player();
+//        player.name = "Tim";
+////        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health =  " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health =  " + player.healthRemaining());
 
-        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
-
-        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
-
-        PC thePC = new PC(theCase, theMonitor, theMotherboard);
-        thePC.powerUp();
-
-
-        // Create a single room of a house using composition.
-        // Think about the things that should be included in the room.
-        // Maybe physical parts of the house but furniture as well
-        // Add at least one method to access an object via a getter and
-        // then that objects public method as you saw in the previous video
-        // then add at least one method to hide the object e.g. not using a getter
-        // but to access the object used in composition within the main class
-        // like you saw in this video.
-
-        Wall wall1 = new Wall("West");
-        Wall wall2 = new Wall("East");
-        Wall wall3 = new Wall("South");
-        Wall wall4 = new Wall("North");
-
-        Ceiling ceiling = new Ceiling(12, 55);
-
-        Bed bed = new Bed("Modern", 4, 3, 2, 1);
-
-        Lamp lamp = new Lamp("Classic", false, 75);
-
-        Bedroom bedRoom = new Bedroom("Tims", wall1, wall2, wall3, wall4, ceiling,bed, lamp);
-        bedRoom.makeBed();
-
-        bedRoom.getLamp().turnOn();
-
-
-
+        EnhancedPlayer player = new EnhancedPlayer("Tim", 200, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
     }
 }
