29d5e048d7399f25429a1fd0e0c59b2d240a8ffb
oszkarpap <scaevola.c@gmail.com>
2019-05-01 09:49:30 +0200

    11. change Hamburger

--- Main.java
+++ Main.java
@@ -1,144 +1,69 @@
 package com.timbuchalka;
 
-class Car {
-    private boolean engine;
-    private int cylinders;
-    private String name;
-    private int wheels;
-
-    public Car(int cylinders, String name) {
-        this.cylinders = cylinders;
-        this.name = name;
-        this.wheels = 4;
-        this.engine = true;
-    }
-
-    public int getCylinders() {
-        return cylinders;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String startEngine() {
-        return "Car -> startEngine()";
-    }
-
-    public String accelerate() {
-        return "Car -> accelerate()";
-    }
-
-    public String brake() {
-        return "Car -> brake()";
-    }
-}
-
-class Mitsubishi extends Car {
-
-    public Mitsubishi(int cylinders, String name) {
-        super(cylinders, name);
-    }
-
-    @Override
-    public String startEngine() {
-        return "Mitsubishi -> startEngine()";
-    }
-
-    @Override
-    public String accelerate() {
-        return "Mitsubishi -> accelerate()";
-    }
-
-    @Override
-    public String brake() {
-        return "Mitsubishi -> brake()";
-    }
-}
-
-class Holden extends Car {
-
-    public Holden(int cylinders, String name) {
-        super(cylinders, name);
-    }
-
-    @Override
-    public String startEngine() {
-        return getClass().getSimpleName() +  " -> startEngine()";
-    }
-
-    @Override
-    public String accelerate() {
-        return getClass().getSimpleName() +  " -> accelerate()";
-    }
-
-    @Override
-    public String brake() {
-        return getClass().getSimpleName() +  " -> brake()";
-    }
-}
-
-class Ford extends Car {
-
-    public Ford(int cylinders, String name) {
-        super(cylinders, name);
-    }
-
-    @Override
-    public String startEngine() {
-        return "Ford -> startEngine()";
-    }
-
-    @Override
-    public String accelerate() {
-        return "Ford -> accelerate()";
-    }
-
-    @Override
-    public String brake() {
-        return "Ford -> brake()";
-    }
-}
-
 public class Main {
 
     public static void main(String[] args) {
-	    // We are going to go back to the car analogy.
-        // Create a base class called Car
-        // It should have a few fields that would be appropriate for a generice car calss.
-        // engine, cylinders, wheels, etc.
-        // Constructor should initialize cylinders (number of) and name, and set wheels to 4
-        // and engine to true. Cylinders and names would be passed parameters.
-        //
-        // Create appropriate getters
-        //
-        // Create some methods like startEngine, accelerate, and brake
-        //
-        // show a message for each in the base class
-        // Now create 3 sub classes for your favorite vehicles.
-        // Override the appropriate methods to demonstrate polymorphism in use.
-        // put all classes in the one java file (this one).
+		// The purpose of the application is to help a ficticious company called Bills Burgers to manage
+		// their process of selling hamburgers.
+		// Our application will help Bill to select types of burgers, some of the additional items (additions) to
+		// be added to the burgers and pricing.
+		// We want to create a base hamburger, but also two other types of hamburgers that are popular ones in
+		// Bills store.
+		// The basic hamburger should have the following items.
+		// bread roll type, meat and up to 4 additional additions (things like lettuce, tomato, carrot, etc) that
+		// the customer can select to be added to the burger.
+		// Each one of these items gets charged an additional
+		// price so you need some way to track how many items got added and to calculate the final price (base
+		// burger with all the additions).
+		// This burger has a base price and the additions are all seperately priced (up to 4 additions, see above).
+		// Create a Hamburger class to deal with all the above.
+		// The constructor should only include the roll type, meat and price, can also include name of burger or you can use setter.
+		// Also create two extra varieties of Hamburgers (subclasses) to cater for
+		// a) Healthy burger (on a brown rye bread roll), plus two addition items can be added.
+		// The healthy burger can have 6 items (Additions) in total.
+		// hint:  you probably want to process the 2 additional items in this new class (subclass of Hamburger), not the base class (Hamburger),
+		// since the 2 additions are only appropriate for this new class (in other words new burger type).
+		// b) Deluxe hamburger - comes with chips and drinks as additions, but no extra additions are allowed.
+		// hint:  You have to find a way to automatically add these new additions at the time the deluxe burger
+		// object is created, and then prevent other additions being made.
+		//  All 3 classes should have a method that can be called anytime to show the base price of the hamburger
+		// plus all additionals, each showing the addition name, and addition price, and a grand/final total for the
+		// burger (base price + all additions)
+		// For the two additional classes this may require you to be looking at the base class for pricing and then
+		// adding totals to final price.
 
-        Car car = new Car(8, "Base car");
-        System.out.println(car.startEngine());
-        System.out.println(car.accelerate());
-        System.out.println(car.brake());
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
 
-        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
-        System.out.println(mitsubishi.startEngine());
-        System.out.println(mitsubishi.accelerate());
-        System.out.println(mitsubishi.brake());
 
-        Ford ford = new Ford(6, "Ford Falcon");
-        System.out.println(ford.startEngine());
-        System.out.println(ford.accelerate());
-        System.out.println(ford.brake());
 
-        Holden holden = new Holden(6, "Holden Commodore");
-        System.out.println(holden.startEngine());
-        System.out.println(holden.accelerate());
-        System.out.println(holden.brake());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-
-
 }
